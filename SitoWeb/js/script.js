fdc77aaa694ccbb47a252d9970cc23196614e9e7
lorenzo <lorenzocorreale2006@gmail.com>
2024-10-04 01:09:50 +0200

    sito web completato

--- SitoWeb/js/script.js
+++ SitoWeb/js/script.js
@@ -1,3 +1,25 @@
-document.getElementById("who-btn").addEventListener("click", function () {
-    window.location.href = "../SitoWeb/about.html"
+document.getElementById("chisono-btn").addEventListener("click", function () {
+    window.location.href = "../SitoWeb/about.html#chat"
 });
+
+document.getElementById("competenze-btn").addEventListener("click", function () {
+    window.location.href = "../SitoWeb/competenze.html#chat"
+});
+
+document.getElementById("esperienze-btn").addEventListener("click", function () {
+    window.location.href = "../SitoWeb/lavori.html#chat"
+});
+
+
+document.getElementById("progetti-btn").addEventListener("click", function () {
+    window.location.href = "../SitoWeb/progetti.html#chat"
+});
+
+document.getElementById("contatti-btn").addEventListener("click", function () {
+    window.location.href = "../SitoWeb/contatti.html#chat"
+});
+
+
+
+
+
